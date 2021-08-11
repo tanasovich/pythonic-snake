7a0e2ba9453067b0fc78aacd09a29ccefff2ebac
Valentine Tanasovich <valentine.tanasovich@gmail.com>
2021-08-11 14:17:12 +0300

    Empty game field

---
+++ main.py
@@ -0,0 +1,15 @@
+import tkinter
+
+FIELD_WIDTH: int = 800
+FIELD_HEIGHT: int = 600
+CELL_SIZE: int = 20
+
+if __name__ == "__main__":
+    root = tkinter.Tk()
+    root.title("Pythonic Snake")
+
+    canvas = tkinter.Canvas(root, width=FIELD_WIDTH, height=FIELD_WIDTH, background="blue")
+
+    canvas.grid()
+
+    root.mainloop()
